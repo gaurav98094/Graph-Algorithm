72812fa73bd34f01fc9bb641198cd8d9592eb514
gauravkandel <kandelgaurav7@gmail.com>
2023-11-04 09:06:40 +0545

    797. All Paths From Source to Target

---
+++ AllPathSrcToTarget.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class AllPathSrcToTarget {
+    public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+        List<List<Integer>> ans = new ArrayList<>();
+        Queue<List<Integer>> q = new LinkedList<>();
+        q.offer(new ArrayList<>(Arrays.asList(0)));
+
+        while (!q.isEmpty()) {
+            List<Integer> path = q.poll();
+            int lastVertex = path.get(path.size() - 1);
+            if (lastVertex == graph.length - 1) {
+                ans.add(path);
+            } else {
+                for (int x : graph[lastVertex]) {
+                    List<Integer> newPath = new ArrayList<>(path);
+                    newPath.add(x);
+                    q.offer(newPath);
+                }
+            }
+        }
+
+        return ans;
+    }
+}
